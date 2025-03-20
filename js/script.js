c0e6d3cc4fabdf040928cbfbe4f8f50817b71ecc
terrespasial <terrespasial@gmail.com>
2025-03-20 14:08:52 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ document.getElementById('logo').addEventListener('mouseenter', function() {
 document.getElementById('logo').addEventListener('mouseleave', function() {
     this.src = './assets/images/ts_logo_bnw.png';  // Kembalikan ke gambar semula
 });
+
+// Toggle the navigation menu on mobile
+const hamburger = document.getElementById('hamburger-icon');
+const navList = document.querySelector('.nav-list');
+
+// Event listener untuk hamburger
+hamburger.addEventListener('click', () => {
+    navList.classList.toggle('show');
+});
