ada2171ae05b6b9f0d907adadf05ac34efba215f
terrespasial <terrespasial@gmail.com>
2025-03-20 16:09:05 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // script.js
-document.getElementById('logo').addEventListener('mouseenter', function() {
+document.getElementById('logo_footer').addEventListener('mouseenter', function() {
     this.src = './assets/images/ts_logo_rgb.png';  // Ganti dengan path gambar baru
 });
 
-document.getElementById('logo').addEventListener('mouseleave', function() {
+document.getElementById('logo_footer').addEventListener('mouseleave', function() {
     this.src = './assets/images/ts_logo_bnw.png';  // Kembalikan ke gambar semula
 });
 
